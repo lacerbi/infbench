344e9d2dc7938d4bcc291ba0f1acd863009f4d14
Luigi Acerbi <luigi.acerbi@nyu.edu>
2018-10-08 16:33:03 +0200

    moved figures

--- matlab/figs/vbmc_nips2018/vbmc_fig1.m
+++ matlab/figs/vbmc_nips2018/vbmc_fig1.m
@@ -3,7 +3,12 @@
 if ~exist('stats','var'); stats = []; end
 plotbnd = [-3,-2; 3,6];
 close all;
-stats = vbmc_demo2d(@rosenbrock_test,stats,plotbnd);
+
+% Define prior
+sigma2 = 9;     % Prior variance
+priorfun = @(x) - 0.5*sum(x.^2,2)/sigma2 - 0.5*size(x,2)*log(2*pi*sigma2);
+
+stats = vbmc_demo2d(@(x) rosenbrock_test(x) + priorfun(x),stats,plotbnd);
 
 pause
 
