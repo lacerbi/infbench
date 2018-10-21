05c40f364fe59fb2209ac5af89605e979a74c810
Luigi Acerbi <luigi.acerbi@nyu.edu>
2018-10-21 22:58:13 +0200

    increased iters

--- matlab/infalgo/wsabi/wsabi_oneshot.m
+++ matlab/infalgo/wsabi/wsabi_oneshot.m
@@ -51,8 +51,8 @@ options1.GradObj                = 'off';
 options1.Algorithm              = 'active-set';
 options1.TolX                   = 1e-5;
 options1.TolFun                 = 1e-5;
-options1.MaxTime                = 0.5;
-options1.MaxFunEvals            = 50;
+options1.MaxTime                = 10;
+options1.MaxFunEvals            = 200*dim;
 %options1.UseParallel           = 'always';
 options1.AlwaysHonorConstraints = 'true';
 
