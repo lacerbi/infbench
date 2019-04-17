c6fdae1fc639b7e9e98c0bd003cdd0b3a5357b34
Luigi Acerbi <luigi.acerbi@nyu.edu>
2019-04-17 17:52:28 +0200

    added algo

--- matlab/infalgo/infalgo_vbmc.m
+++ matlab/infalgo/infalgo_vbmc.m
@@ -90,6 +90,7 @@ switch algoset
         algoptions.WarmupOptions = w;
         algoptions.TolStableWarmup = 5; algoptions.BOWarmup = true; algoptions.NSgpMaxWarmup = 8;
     case {42,'robust'}; algoset = 'robust'; algoptions.NSgpMaxMain = 0; algoptions.GPStochasticStepsize = true; algoptions.WarmupNoImproThreshold = 20 + 5*numel(probstruct.InitPoint);
+    case {43,'robust2'}; algoset = 'robust2'; algoptions.NSgpMaxMain = 0; algoptions.GPStochasticStepsize = true; algoptions.WarmupNoImproThreshold = 20 + 5*numel(probstruct.InitPoint); algoptions.TolStableExceptions = 2; algoptions.TolStableIters = 10;
         
     % Variational active sampling
     case {100,'vas'}; algoset = 'vas'; 
