ead09e0bcde09fd4a3f8b385842cb19b0260f59b
Luigi Acerbi <luigi.acerbi@nyu.edu>
2019-04-21 12:23:13 +0200

    new defaults

--- matlab/infalgo/infalgo_vbmc.m
+++ matlab/infalgo/infalgo_vbmc.m
@@ -92,6 +92,7 @@ switch algoset
     case {42,'robust'}; algoset = 'robust'; algoptions.NSgpMaxMain = 0; algoptions.GPStochasticStepsize = true; algoptions.WarmupNoImproThreshold = 20 + 5*numel(probstruct.InitPoint);
     case {43,'robust2'}; algoset = 'robust2'; algoptions.NSgpMaxMain = 0; algoptions.GPStochasticStepsize = true; algoptions.WarmupNoImproThreshold = 20 + 5*numel(probstruct.InitPoint); algoptions.TolStableExceptions = 2; algoptions.TolStableIters = 10;
     case {44,'robust3'}; algoset = 'robust3'; algoptions.NSgpMaxMain = 0; algoptions.GPStochasticStepsize = true; algoptions.WarmupNoImproThreshold = 20 + 5*numel(probstruct.InitPoint); algoptions.TolStableExceptions = 2; algoptions.TolStableIters = 10; algoptions.WarmupCheckMax = true;
+    case {45,'newdef'}; algoset = 'robust4'; algoptions.WarmupNoImproThreshold = 20 + 5*numel(probstruct.InitPoint); algoptions.TolStableExceptions = 2; algoptions.TolStableIters = 10; algoptions.WarmupCheckMax = true;
         
     % Variational active sampling
     case {100,'vas'}; algoset = 'vas'; 
