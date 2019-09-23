6ad4a130ce5c0a124e8ee691416c68d4840ec9fa
lacerbi <luigi.acerbi@gmail.com>
2019-09-23 08:09:59 +0200

    fixed posterior mode

--- matlab/problems/infbench_funnel.m
+++ matlab/problems/infbench_funnel.m
@@ -47,7 +47,14 @@ if isempty(x)
     post_var(2:D) = integral(fun,y.PLB(1)-range(1),y.PUB(1)+range(1))/Z;
     
     y.Post.Cov = diag(post_var);
-    y.Post.Mode = y.Post.Mean;
+    
+    % Compute posterior mode analytically
+    tau2 = sigma_prior^2*sigma^2/(sigma^2 + sigma_prior^2);
+%     fun = @(x) 0.5*x(:,1).^2/y.sigma2 ...
+%         + 0.5*sum(x(:,2:D).^2,2)./exp(x(:,1)) + 0.5*(D-1)*x(:,1) ...
+%         + 0.5*sum(x.^2,2)/sigma_prior^2;
+%     Mode = fminunc(fun,zeros(1,D));   % Numerical check
+    y.Post.Mode = [-0.5*(D-1)*tau2,zeros(1,D-1)];
         
 else
     sigma2 = infprob.sigma2;
