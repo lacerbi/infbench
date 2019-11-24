62fcedad2186df386fb361e4570f52d34cebd9ae
lacerbi <luigi.acerbi@gmail.com>
2019-11-24 11:36:42 +0100

    algos

--- matlab/infbench_defaults.m
+++ matlab/infbench_defaults.m
@@ -133,7 +133,7 @@ switch lower(type)
                         % VBMC with noisy functions
                     case 'dtstep1_99'; defaults.color = [180 0 80]/255; defaults.marker = '^'; defaults.linewidth = 2; defaults.linestyle = '-';
                     case {'dtstep1_100','ent1'}; defaults.color = [120 100 0]/255; defaults.marker = ''; defaults.linewidth = 3; defaults.linestyle = '-';
-                    case {'acqmidtstep1_100','ent2'}; defaults.color = [250 0 80]/255; defaults.marker = '+'; defaults.linewidth = 2; defaults.linestyle = '-';
+                    case {'acqmidtstep1_100','ent2','ent2midtstep1mc'}; defaults.color = [250 0 80]/255; defaults.marker = '+'; defaults.linewidth = 2; defaults.linestyle = '-';
                     case {'ent3','ent2midtmcmc50','ent2midtmcmc50step1'}; defaults.color = [250 0 80]/255; defaults.marker = '+'; defaults.linewidth = 2; defaults.linestyle = ':';
                     case 'ent2midtstep1'; defaults.color = [180 0 80]/255; defaults.marker = '^'; defaults.linewidth = 2; defaults.linestyle = '--';
                     case {'ent1midtstep1'}; defaults.color = [120 100 0]/255; defaults.marker = ''; defaults.linewidth = 3; defaults.linestyle = ':';
