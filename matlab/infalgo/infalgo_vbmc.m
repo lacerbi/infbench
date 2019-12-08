369302b9551bcd2295343d6c1220c4ff707ba53e
lacerbi <luigi.acerbi@gmail.com>
2019-12-08 10:50:48 +0000

    added algo

--- matlab/infalgo/infalgo_vbmc.m
+++ matlab/infalgo/infalgo_vbmc.m
@@ -192,6 +192,7 @@ switch algoset
     case {83,'gpfast6'}; algoset = 'gpfast6'; algoptions = newdefaults; algoptions.SearchCacheFrac = 0.01;
     case {84,'gpfast7'}; algoset = 'gpfast7'; algoptions = newdefaults; algoptions.SearchCacheFrac = 0.01; algoptions.NSgpMaxWarmup = 0; algoptions.NSgpMaxMain = 0;
     case {85,'gpfast8'}; algoset = 'gpfast8'; algoptions = newdefaults; algoptions.NSgpMaxWarmup = 2; algoptions.NSgpMaxMain = 2;
+    case {86,'gpfast9'}; algoset = 'gpfast9'; algoptions = newdefaults; algoptions.NSgpMaxWarmup = 2; algoptions.NSgpMaxMain = 2; algoptions.GPTolOptMCMC = 1e-5;
     
     % New defaults
     case {100,'newdef'}; algoset = 'newdef'; algoptions = newdefaults;
