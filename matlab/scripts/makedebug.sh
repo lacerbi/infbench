72565bdc360c5e393e6e932a550b84349bf521f1
Acerbi Luigi <acerbi@turso01.cs.helsinki.fi>
2022-07-07 10:00:54 +0300

    refactor: moved scripts to folders

--- matlab/scripts/makedebug.sh
+++ matlab/scripts/makedebug.sh
@@ -1,29 +1,61 @@
 #!/bin/sh
+echo "Usage: makejobs job# [file#]"
 
-PROJECT="neurobench"
+PROJECT="infbench"
 
 module purge
-#. /etc/profile.d/modules.sh
 
 # Use Intel compiler
-module load matlab
+module load ${MATLAB_MODULE}
 source ${HOME}/MATLAB/setpath.sh
-
 export MATLABPATH=${MATLABPATH}
-FILENAME="'${1}'"
+WORKDIR="${SCRATCH}/${PROJECT}"
+
+if [ -z "${2}" ];
+	then DIRID=${1};
+	else DIRID=${2};
+fi
+
+FILEID=${1}
+FILENAME="joblist-${FILEID}.txt"
+echo "Input #: ${1}   Output file: ${FILENAME}"
+
+VBMC18="{'lumpy','studentt','cigar'}"
+
+# Default job list
+PROBSET="'vbmc18'"
+PROBS=${VBMC18}
+DIMS="{'2D','6D','10D'}"
+NOISE="'[]'"
+ALGOS="{'vbmc'}"
+ALGOSET="'base'"
+IDS="{'1:2','3:4','5:6','7:8','9:10','11:12','13:14','15:16','17:18','19:20'}"
+IDS_SINGLE="{'1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'}"
+IDS_FIFTY="{'1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50'}"
+IDS_CENTO="{'1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60','61','62','63','64','65','66','67','68','69','70','71','72','73','74','75','76','77','78','79','80','81','82','83','84','85','86','87','88','89','90','91','92','93','94','95','96','97','98','99','100'}"
+
+##############################################################################
 
 # Run the program
-PROBS="{1}"
-DIMS="{2,3,5,10,20}"
-NOISE="'[]'"
-ALGOS="{'fmincon','patternsearch'}"
-IDS="'1:10'"
+case "${1}" in
+	0)      PROBS="{'lumpy'}"
+		ALGOS="{'vbmc'}"
+		DIMS="{'2D','4D'}"
+		IDS="{'1','2'}"
+		;;
+esac
+
+##############################################################################
+
+echo "Job items: ${PROBSET},${PROBS},${DIMS},${NOISE},${ALGOS},${ALGOSET},${IDS}"
 
 cat<<EOF | matlab -nodisplay
-parlist=combcell('cec14',${PROBS},${DIMS},${NOISE},${ALGOS},'base',${IDS});
-fout=fopen(${FILENAME},'w+');
-for i=1:length(parlist)
-	fprintf(fout,'''%s'',''%d'',''%d'',''%s'',''%s'',''%s'',''%s''\n',parlist{i}{1},parlist{i}{2},parlist{i}{3},parlist{i}{4},parlist{i}{5},parlist{i}{6},parlist{i}{7});
-end
-fclose(fout);
+addpath(genpath('${HOME}/${PROJECT}'));
+currentDir=cd;
+cd('${WORKDIR}');
+infbench_joblist('${FILENAME}','run${DIRID}',${PROBSET},${PROBS},${DIMS},${NOISE},${ALGOS},${ALGOSET},${IDS});
+cd(currentDir);
 EOF
+
+cat ${WORKDIR}/${FILENAME}
+cat ${WORKDIR}/${FILENAME} | wc
--- matlab/scripts/oldMATLABrun.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-module purge
-module load matlab/2014a
-BASEPATH="${ROOTPATH}/MATLAB"
-source ${BASEPATH}/setpath.sh
-export MATLABPATH=${MATLABPATH}:${HOME}/MATLAB
-matlab -nodisplay
